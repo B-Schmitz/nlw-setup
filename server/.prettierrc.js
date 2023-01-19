71874665c7cc16adff019744a4b5e0d46f3ddb62
B-Schmitz <bernardoschmitz0@gmail.com>
2023-01-19 19:50:34 -0300

    aula 01

---
+++ server/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 4,
+  singleQuote: true,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  semi: true,
+  jsxSingleQuote: true,
+  printWidth: 180,
+  arrowParens: 'avoid'
+};
---
+++ web/tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,j,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
